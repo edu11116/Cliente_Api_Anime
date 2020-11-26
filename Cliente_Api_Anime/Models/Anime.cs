902c033d04286b2c68efdd28def707dbe253e944
edu11116 <ed.aguirrem@alumnos.duoc.cl>
2020-11-26 16:25:47 -0300

    crear_clase
    crear_clase

---
+++ Cliente_Api_Anime/Models/Anime.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cliente_Api_Anime.Models
+{
+    public class Anime
+    {
+    }
+}
