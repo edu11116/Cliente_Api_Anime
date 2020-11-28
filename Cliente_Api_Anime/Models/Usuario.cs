5d2a6470669e6313c30a6bd0f3c1db3f1d5ed5d0
AleViera <ale.viera@alumnos.duoc.cl>
2020-11-27 23:46:49 -0300

    reacher clase usuario
    rehacer clase usuario

--- Cliente_Api_Anime/Models/Usuario.cs
+++ Cliente_Api_Anime/Models/Usuario.cs
@@ -5,10 +5,10 @@ using System.Threading.Tasks;
 
 namespace Cliente_Api_Anime.Models
 {
-    public class usuario
+    public class Usuario
     {
         public int id_usuario { get; set; }
-        public string nombre_usuario { get; set; } 
+        public string nombre_usuario { get; set; }
         public string pass { get; set; }
         public string tipo_usuario { get; set; }
     }
