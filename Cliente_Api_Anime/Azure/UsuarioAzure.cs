8aded50b8592fb285de019ce653198c74380aa61
AleViera <ale.viera@alumnos.duoc.cl>
2020-12-01 12:34:51 -0300

    Se encapsula el ObtenerUsuario y se agregan los metodos LlenarUsuario, LlenarDaraTable, ConsultaSqlUsuario

--- Cliente_Api_Anime/Azure/UsuarioAzure.cs
+++ Cliente_Api_Anime/Azure/UsuarioAzure.cs
@@ -17,39 +17,54 @@ namespace Cliente_Api_Anime.Azure
         //OBTENER USUARIOS
         public static List<Usuario> ObtenerUsuarios()
         {
-            var dataTable = new DataTable();
-
             using (SqlConnection connection = new SqlConnection(connectionString))
             {
-                
-                SqlCommand sqlCommand = new SqlCommand(null, connection);
-                sqlCommand.CommandText = "select * from usuario";
-
-                connection.Open();
-                var DataAdapter = new SqlDataAdapter(sqlCommand);
-
-                DataAdapter.Fill(dataTable);
-
-                usu = new List<Usuario>();
-
-                //COn este for hacemos que ingrese un id la cantidad de veces segun las filas que encuentre
-                for (int i = 0; i < dataTable.Rows.Count; i++)
-                {
-                    Usuario usuario = new Usuario();
-                    usuario.id_usuario = int.Parse(dataTable.Rows[i]["id_usuario"].ToString());
-                    usuario.nombre_usuario = dataTable.Rows[i]["nombre_usuario"].ToString();
-                    usuario.pass = dataTable.Rows[i]["pass"].ToString();
-                    usuario.tipo_usuario = dataTable.Rows[i]["tipo_usuario"].ToString();
-
-                    usu.Add(usuario);
-                    
-                }
-
-
-
+                var consultaSql = "select * from Usuario";
+                var comando = ConsultaSqlUsuario(connection, consultaSql);
+                var dataTableUsuarios = LlenarDataTable((SqlCommand)comando);//la conversion explicita no se si esta correcta((SqlCommand)comando)
+                return LlenadoUsuario(dataTableUsuarios);
             }
+            
+        }
+
+
+        private static object ConsultaSqlUsuario(SqlConnection connection, string consulta)
+        {
+            SqlCommand sqlCommand = new SqlCommand(null, connection);
+            sqlCommand.CommandText = consulta;
+            connection.Open();
+            return sqlCommand;
+        }
+
+        public static DataTable LlenarDataTable(SqlCommand comando)
+        {
+           
+            var dataTable = new DataTable();
+            var dataAdapter = new SqlDataAdapter(comando);
+            dataAdapter.Fill(dataTable);
+            return dataTable;
+        }
+
+        private static List<Usuario> LlenadoUsuario(DataTable dataTable)
+        {
+            usu = new List<Usuario>();
+
+            //COn este for hacemos que ingrese un id la cantidad de veces segun las filas que encuentre
+            for (int i = 0; i < dataTable.Rows.Count; i++)
+            {
+                Usuario usuario = new Usuario();
+                usuario.id_usuario = int.Parse(dataTable.Rows[i]["id_usuario"].ToString());
+                usuario.nombre_usuario = dataTable.Rows[i]["nombre_usuario"].ToString();
+                usuario.pass = dataTable.Rows[i]["pass"].ToString();
+                usuario.tipo_usuario = dataTable.Rows[i]["tipo_usuario"].ToString();
+
+                usu.Add(usuario);
+            }
+
 
             return usu;
         }
+
+
     }
 }
